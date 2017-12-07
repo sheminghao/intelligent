6749420e8244583e9d8160955b8a44ab920e9bdf
shemh <18664565413@163.com>
2017-12-07 16:35:36 +0800

    登录界面

--- app/src/main/java/com/shemh/intelligent/LoginActivity.java
+++ app/src/main/java/com/shemh/intelligent/LoginActivity.java
@@ -1,13 +1,16 @@
 package com.shemh.intelligent;
 
+import android.app.ProgressDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import android.widget.RelativeLayout;
 
 import com.shemh.intelligent.utils.AppUtils;
 
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener{
 
     RelativeLayout layoutLogin;
 
@@ -19,6 +22,18 @@ public class LoginActivity extends AppCompatActivity {
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
         layoutLogin = (RelativeLayout) findViewById(R.id.content_login);
         layoutLogin.setPadding(0, AppUtils.getStatusBar(), 0, 0);
+
+        Button btnLogin = (Button) findViewById(R.id.btn_login);
+        btnLogin.setOnClickListener(this);
     }
 
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.btn_login://登录
+                ProgressDialog progressDialog = new ProgressDialog(this);
+                progressDialog.show();
+                break;
+        }
+    }
 }
