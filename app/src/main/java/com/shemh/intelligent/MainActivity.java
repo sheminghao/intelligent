a9bb304c3906ee7eefc95cf2bf846d4c64acbc62
shemh <18664565413@163.com>
2017-12-08 16:45:22 +0800

    首页

--- app/src/main/java/com/shemh/intelligent/MainActivity.java
+++ app/src/main/java/com/shemh/intelligent/MainActivity.java
@@ -5,14 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.KeyEvent;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import com.shemh.intelligent.adapter.SeatAdapter;
 import com.shemh.intelligent.utils.ToastUtils;
+import com.shemh.intelligent.view.SeatTable;
 
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<String> list;
+    SeatAdapter seatAdapter;
+    GridLayoutManager gridLayoutManager;
+    public SeatTable seatTableView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,16 +29,74 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         RecyclerView recyclerview = (RecyclerView) findViewById(R.id.recyclerview);
-        SeatAdapter seatAdapter = new SeatAdapter(this);
-        GridLayoutManager gridLayoutManager = new GridLayoutManager(this, 3);
+        seatAdapter = new SeatAdapter(this);
+        gridLayoutManager = new GridLayoutManager(this, 3);
         recyclerview.setLayoutManager(gridLayoutManager);
         recyclerview.setAdapter(seatAdapter);
 
-        List<String> list = new ArrayList<>();
+        list = new ArrayList<>();
         for (int i = 0; i < 45; i++) {
             list.add(i + "");
         }
         seatAdapter.setDataList(list);
+
+        final EditText etRow = (EditText) findViewById(R.id.et_row);
+        final EditText etNum = (EditText) findViewById(R.id.et_num);
+        TextView tvQueding = (TextView) findViewById(R.id.tv_queding);
+        tvQueding.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                int row = Integer.parseInt(etRow.getText().toString().trim());
+                int num = Integer.parseInt(etNum.getText().toString().trim());
+                gridLayoutManager.setSpanCount(row);
+                list.clear();
+                for (int i = 0; i < num; i++) {
+                    list.add(i + "");
+                }
+                seatAdapter.setDataList(list);
+            }
+        });
+
+
+        seatTableView = (SeatTable) findViewById(R.id.seatView);
+        seatTableView.setScreenName("8号厅荧幕");//设置屏幕名称
+        seatTableView.setMaxSelected(3);//设置最多选中
+
+        seatTableView.setSeatChecker(new SeatTable.SeatChecker() {
+
+            @Override
+            public boolean isValidSeat(int row, int column) {
+                if(column==2) {
+                    return false;
+                }
+                return true;
+            }
+
+            @Override
+            public boolean isSold(int row, int column) {
+                if(row==6&&column==6){
+                    return true;
+                }
+                return false;
+            }
+
+            @Override
+            public void checked(int row, int column) {
+
+            }
+
+            @Override
+            public void unCheck(int row, int column) {
+
+            }
+
+            @Override
+            public String[] checkedSeatTxt(int row, int column) {
+                return null;
+            }
+
+        });
+        seatTableView.setData(10,15);
     }
 
     private long exitTime = 0;
